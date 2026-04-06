a94c580f9b4df4ecfe1c061038c324f009ccf4ee
GreeneJK <jeremy.greene@wheniwork.com>
2026-04-05 20:06:28 -0500

    Fix TrustBar: replace corrupt emojis with HTML entities

--- src/components/TrustBar.jsx
+++ src/components/TrustBar.jsx
@@ -3,10 +3,11 @@ export default function TrustBar() {
     <div className="trust-bar">
       <div className="container">
         <div className="trust-bar-inner">
-          <div className="trust-item"><span className="icon">ð§</span> Free Monthly Newsletter</div>
-          <div className="trust-item"><span className="icon">ð</span> Free Trip Consultation</div>
-          <div className="trust-item"><span className="icon">ð</span> Rochester, MN</div>
-          <div className="trust-item"><span className="icon">â­</span> 5-Star Reviews</div>
+          <div className="trust-item"><span className="icon">&#x1F3F0;</span> Authorized Disney Vacation Planner</div>
+          <div className="trust-item"><span className="icon">&#x1F4E7;</span> Free Monthly Newsletter</div>
+          <div className="trust-item"><span className="icon">&#x1F4C5;</span> Free Trip Consultation</div>
+          <div className="trust-item"><span className="icon">&#x1F4CD;</span> Rochester, MN</div>
+          <div className="trust-item"><span className="icon">&#x2B50;</span> 5-Star Reviews</div>
         </div>
       </div>
     </div>
